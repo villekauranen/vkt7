e1a515e27bb0947fdca455e0776aa94640390b3b
villekauranen <44529976+villekauranen@users.noreply.github.com>
2018-11-22 17:18:09 +0200

    3-4: Using a Database (with Mongoose)

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -39,3 +39,11 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://<seppo>:<salaperainen209>@ds163842.mlab.com:63842/paikallinen_kikkare';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
